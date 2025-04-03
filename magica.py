ed1ddcc6916119835d4e90004dde835cf6d744df
cheycattani <cheycattani@gmail.com>
2025-04-03 18:58:30 -0300

    lógica de programação

---
+++ magica.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdout.reconfigure(encoding="utf-8")
+
+import os
+
+os.system('clear')
+
+print('Olhe as cartas abaixo e escolha uma: ')
+print('---')
+print('Q♣️  K♦️  J♥️  Q♥️  J♣️  K♠️')
+input()
+
+os.system('clear')
+
+print('A carta que você pensou...')
+input()
+
+print()
+print('Desapareceu!')
+print()
+
+print('J♠️  Q♦️  J♦️  Q♠️  K♥️')
