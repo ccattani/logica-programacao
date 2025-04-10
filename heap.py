5624cec711bf17243bb8c96f659c907640a82030
cheycattani <cheycattani@gmail.com>
2025-04-09 23:26:47 -0300

    heap

---
+++ heap.py
@@ -0,0 +1,13 @@
+from heapq import * 
+
+fila_prioridade = []
+
+heappush(fila_prioridade, (2, 'Atender cliente VIP'))
+heappush(fila_prioridade, (1, 'Situação crítica'))
+heappush(fila_prioridade, (3, 'Responder e-mails'))
+heappush(fila_prioridade, (1, 'Apagar incêndio'))
+
+while fila_prioridade:
+    prioridade, tarefa = heappop(fila_prioridade)
+    print(f'Executando: {tarefa} - Prioridade: {prioridade}')
+
