eb2064e55342b6b903be73900dae0221fb786cd0
cheycattani <cheycattani@gmail.com>
2025-04-09 21:08:33 -0300

    pilha e fila

---
+++ pilha.py
@@ -0,0 +1,20 @@
+def criaPilha():
+    return []
+
+def insereNaPilha(pilha, elemento):
+    pilha.append(elemento)
+
+def removeDaPilha(pilha):
+    return pilha.pop()
+
+pilha = criaPilha()
+
+print(pilha)
+insereNaPilha(pilha, 8)
+insereNaPilha(pilha, 9)
+insereNaPilha(pilha, 10)
+insereNaPilha(pilha, 11)
+insereNaPilha(pilha, 12)
+print(f'Removendo: {removeDaPilha(pilha)}')
+print(f'Removendo: {removeDaPilha(pilha)}')
+print(pilha)
